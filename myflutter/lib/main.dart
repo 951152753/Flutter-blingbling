5d429e98ec02b5219fabff81dff132a55e72d36d
yangkexin <yangkexin@winsense.ai>
2020-08-12 21:52:33 +0800

    官网教程结束

--- myflutter/lib/main.dart
+++ myflutter/lib/main.dart
@@ -3,7 +3,7 @@
  * @Author: yangkexin
  * @Date: 2020-08-07 19:15:02
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-08-12 16:06:36
+ * @LastEditTime: 2020-08-12 21:46:50
  */
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart'; // 引入包
@@ -15,32 +15,116 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold( 
-      // Scaffold是Material library中提供的一个widget, 它提供了默认的导航栏、标题和包含主屏幕widget树的body属性。widget树可以很复杂。
-      // widget的主要工作是提供一个build()方法来描述如何根据其他较低级别的widget来显示自己。
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-          centerTitle: true, // 标题居中
-        ),
-        body: new Center(
-          // body的widget树中包含了一个Center widget,
-          // Center widget又包含一个 Text 子widget。
-          // Center widget可以将其子widget树对其到屏幕中心。
-          
-          child: new RandomWords(),
-        ),
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: new RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+        // 在偶数行，该函数会为单词对添加一个ListTile row.
+        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) return new Divider();
+
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对
+          if (index >= _suggestions.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+        centerTitle: true, // 标题居中
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      // Navigator.push 会使路由入栈（导航管理器的栈）
+      new MaterialPageRoute(
+        // 新页面的内容在 MaterialPageRoute 的 builder属性中构建，
+        builder: (context) {
+          // builder是一个匿名函数。
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          // 在每个ListTile之间添加1像素的分割线。
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          // builder返回一个Scaffold，其中包含名为“Saved Suggestions”的新路由的应用栏。
+          // 新路由的body由包含ListTiles行的ListView组成; 每行之间通过一个分隔线分隔。
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
       ),
     );
   }
 }
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
-  }
-}
+
 class RandomWords extends StatefulWidget {
   @override
   createState() => new RandomWordsState();
